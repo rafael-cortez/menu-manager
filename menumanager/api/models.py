e79527c6e91ec6a69fe17081679dde420775a3b3
Rafael Bradaschia Cortez <vcortra>
2019-04-03 16:27:45 -0300

    Created APIs and populate default content

--- menumanager/api/models.py
+++ menumanager/api/models.py
@@ -1,1 +1,25 @@
-# Create your models here.
+from django.db import models
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=128)
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+
+class DefaultSandwich(models.Model):
+    name = models.CharField(max_length=128)
+    ingredient = models.ManyToManyField(Ingredient, related_name='ingredients')
+
+    def __str__(self):
+        return self.name
+
+
+class Promotion(models.Model):
+    name = models.CharField(max_length=128)
+    desc = models.CharField(max_length=1024)
+
+    def __str__(self):
+        return self.name
